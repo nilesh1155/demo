bbefed454678760f09cf6a4369f7e0d020b75cb0
nilesh1155 <nileshprof2015@gmail.com>
2023-09-14 16:28:54 +0530

    [nilesh] This is demo project

---
+++ src/xyz/Demo.java
@@ -0,0 +1,13 @@
+package xyz;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("this is demo git project");
+	}
+
+}
